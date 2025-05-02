9dfdfc6db9d12db6896a356cfef843170de9eb40
Chen Weizhi <164135380+d2d-weizhi@users.noreply.github.com>
2025-05-02 19:15:34 +0800

    Using styles instead.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,15 +12,30 @@ export default function Dashboard() {
       <div className="flex justify-center items-center gap-8 p-8">
         <div className="flex flex-col items-center justify-center w-1/3 aspect-square rounded-lg bg-gray-100 p-8">
           <span className="text-xs font-medium text-gray-600">Avg. Grade</span>
-          <span className="text-4xl font-bold">B+</span>
+          <span 
+            styles={{
+              fontSize: "2.5rem",
+              fontWeight: "300"
+            }}
+          >B+</span>
         </div>
         <div className="flex flex-col items-center justify-center w-1/3 aspect-square rounded-lg bg-gray-100 p-8">
           <span className="text-xs font-medium text-gray-600">Submission Rating</span>
-          <span className="text-4xl font-bold">78.8%</span>
+          <span
+            styles={{
+              fontSize: "2.5rem",
+              fontWeight: "300"
+            }}  
+          >78.8%</span>
         </div>
         <div className="flex flex-col items-center justify-center w-1/3 aspect-square rounded-lg bg-gray-100 p-8">
           <span className="text-xs font-medium text-gray-600">Appraisal Rating</span>
-          <span className="text-4xl font-bold">4.1</span>
+          <span
+            styles={{
+              fontSize: "2.5rem",
+              fontWeight: "300"
+            }}  
+          >4.1</span>
         </div>
       </div>
     </div>
