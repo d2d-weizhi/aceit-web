41798b4d1ed7ccc75b326219d92a0367d79fcfd1
Chen Weizhi <164135380+d2d-weizhi@users.noreply.github.com>
2025-05-02 19:10:39 +0800

    Correct class to className.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,18 +9,18 @@ export default function Dashboard() {
         {/* Put our student profile picture here. */}
       </div>
 
-      <div class="flex justify-center items-center gap-4 p-4">
-        <div class="flex flex-col items-center justify-center w-1/3 aspect-square rounded-lg bg-gray-100 p-4">
-          <span class="text-xs font-medium text-gray-600">Avg. Grade</span>
-          <span class="text-2xl font-bold">B+</span>
+      <div className="flex justify-center items-center gap-4 p-4">
+        <div className="flex flex-col items-center justify-center w-1/3 aspect-square rounded-lg bg-gray-100 p-4">
+          <span className="text-xs font-medium text-gray-600">Avg. Grade</span>
+          <span className="text-2xl font-bold">B+</span>
         </div>
-        <div class="flex flex-col items-center justify-center w-1/3 aspect-square rounded-lg bg-gray-100 p-4">
-          <span class="text-xs font-medium text-gray-600">Submission Rating</span>
-          <span class="text-2xl font-bold">78.8%</span>
+        <div className="flex flex-col items-center justify-center w-1/3 aspect-square rounded-lg bg-gray-100 p-4">
+          <span className="text-xs font-medium text-gray-600">Submission Rating</span>
+          <span className="text-2xl font-bold">78.8%</span>
         </div>
-        <div class="flex flex-col items-center justify-center w-1/3 aspect-square rounded-lg bg-gray-100 p-4">
-          <span class="text-xs font-medium text-gray-600">Appraisal Rating</span>
-          <span class="text-2xl font-bold">4.1</span>
+        <div className="flex flex-col items-center justify-center w-1/3 aspect-square rounded-lg bg-gray-100 p-4">
+          <span className="text-xs font-medium text-gray-600">Appraisal Rating</span>
+          <span className="text-2xl font-bold">4.1</span>
         </div>
       </div>
     </div>
