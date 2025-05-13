f561583444f6597c23477261ed307c88992bb65c
d2d.weizhi <d2d.weizhi@gmail.com>
2025-05-13 15:35:16 +0800

    Applying the SideDrawer and button to the Tasks Page.

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import { useState } from "react";
-// import SideDrawer from "@/shared/components/sidedrawer";
+import Image from "next/image";
+import { Menu, X } from "lucide-react";
+import SideDrawer from "@/shared/components/sidedrawer";
 
 export default function Tasks() {
+	const [isShowMenu, setIsShowMenu] = useState<boolean>(false);
 	const [isShowRightPanel, setIsShowRightPanel] = useState(false);
 	
 		return (
@@ -15,11 +18,42 @@ export default function Tasks() {
 						transitionDelay: isShowRightPanel ? '0ms' : '250ms' // Delay for hiding
 					}}
 				> 
-					
-	
-					<button type="button" onClick={() => setIsShowRightPanel(!isShowRightPanel)}>
-						Show Right Panel
-					</button>
+					{/* Top Bar starts here. */}
+						<div className="flex flex-row items-center p-2 h-max z-50">
+							<div className="flex-1 flex-col">
+								<button
+									type="button"
+									onClick={() => {
+										setIsShowMenu(!isShowMenu);
+									}}
+									className="fixed top-0 left-0 m-6 z-50"
+								>
+									{isShowMenu ? (
+										<X width={64} height={64} strokeWidth={1.5} color={"#FEFEFE"}
+											className={`menu-button ${ isShowMenu ? 'fade-in' : 'fade-out' }`}
+										/>
+									) : (
+										<Menu width={64} height={64} strokeWidth={1.5} color={"#141414"}
+											className={`menu-button ${ isShowMenu ? 'fade-out' : 'fade-in' }`}
+										/>
+									)}
+								</button>
+								<SideDrawer isShow={isShowMenu} />
+							</div>
+							<div className="flex w-auto p-2">
+								<Image src="/green-monster.png" alt={"Sample Profile Image"} width={48} height={48} className="rounded-full" />
+							</div>
+						</div>
+						{/* Top Bar ends here. */}
+
+						{/* Left Panel content starts here. */}
+						<div className="flex flex-row items-center p-2 h-max z-10">
+							{/* We will put this here to prevent a linting error on vercel. */}
+							<button onClick={() => setIsShowRightPanel(!isShowRightPanel)}>
+								Show Right Panel
+							</button>
+						</div>
+						{/* Left Panel content ends here. */}
 					
 				</div>
 				<div 
